3f34dae1c2666ad9c187f8a118a8a6bd11cf0f8e
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-28 21:58:42 +0200

    add columns

--- database/migrations/2023_06_28_124242_create_trains_table.php
+++ database/migrations/2023_06_28_124242_create_trains_table.php
@@ -15,7 +15,18 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+
+            $table->string('azienda, 50');
+            $table->string('stazione_partenza, 50');
+            $table->string('stazione_arrivo, 50');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->char('codice_treno, 10');
+            $table->tinyInteger('numero_carrozze');
+            $table->string('puntuale')->default(true);
+            $table->string('cancellato')->default(false);
+
+            $table->$table->timestamps();
         });
     }
 
